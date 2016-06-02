5ca156cfe420048d65ae5251dee6450cf916a621
Chen Fati <fati.chen@etu.umontpellier.fr>
2016-06-02 14:47:21 +0200

    init

---
+++ Generator.h
@@ -0,0 +1,113 @@
+/*
+ * Created by stardisblue on 12/02/16.
+ */
+
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#include <stdlib.h>
+#include <time.h>
+
+#include "Jeu.h"
+#include "Corolle.h"
+#include "File/FileOut.h"
+
+
+class Generator
+{
+public:
+    static const int PARCOURS_COROLLE = 0;
+    static const int PARCOURS_ROW = 1;
+    static const int PARCOURS_DIAGONAL = 2;
+    static const int PARCOURS_SPIRALE_IN = 3;
+    static const int PARCOURS_MAX = 4;
+
+    Generator(Jeu jeu);
+
+    void multipleGeneration();
+
+    void parcoursBruteForce(const int type_parcours);
+
+private:
+
+    static const int POS_X = 0;
+    static const int POS_Y = 1;
+    static const int POS_TYPE = 2;
+
+    static const int POS_TYPE_COIN_NW = 1;
+    static const int POS_TYPE_COIN_NE = 2;
+    static const int POS_TYPE_COIN_SE = 3;
+    static const int POS_TYPE_COIN_SW = 4;
+    static const int POS_TYPE_COIN = 5;
+
+    static const int POS_TYPE_BORD = 10;
+    static const int POS_TYPE_BORD_TOP = 11;
+    static const int POS_TYPE_BORD_LEFT = 12;
+    static const int POS_TYPE_BORD_BOTTOM = 13;
+    static const int POS_TYPE_BORD_RIGHT = 14;
+
+    static const int POS_TYPE_INTERIEUR = 6;
+
+    static const int JEU_SIZE_MAX = 16;
+    static const int JEU_PIECES_MAX = 256;
+
+    static const int SE = 0;
+    static const int SW = 1;
+    static const int NW = 2;
+    static const int NE = 3;
+
+    static const int N = 0;
+    static const int E = 1;
+    static const int S = 2;
+    static const int W = 3;
+
+    clock_t first_solution;
+
+    Jeu jeu;
+
+    int jeu_size;
+    Piece plateau[JEU_SIZE_MAX][JEU_SIZE_MAX];
+
+    bool disponibles[JEU_PIECES_MAX];
+    int coordonnees[100][3];
+
+    int corolle_size;
+    int corolle_hamming;
+
+    long long nb_noeuds;
+    ostringstream nb_noeuds_first;
+    long long nb_solutions;
+
+    void diagonalWalker(int &position_nb, int &x, int &y, const int orientation, const int &length);
+
+    void straightWalker(int &position_nb, int &x, int &y, const int orientation, const int &length);
+
+    void coordinatesCreator(int x, int y, const int type_parcours);
+
+    void addCoordinate(int &position_nb, const int &x, const int &y);
+
+    const int pieceTypeByPosition(const int &x, const int &y);
+
+    const bool compareColors(Piece &a, Piece &b, const int side_a, const int side_b);
+
+    const bool compareSides(Piece &piece, const int &x, const int &y, const int side_to_compare);
+
+    const bool canPutPiece(Piece &piece, const int &x, const int &y, const int position_type);
+
+    void putPiece(const int &x, const int &y, Piece &piece);
+
+    void pickOffPiece(const int &numero_piece, const int &x, const int &y);
+
+    void generationRecursive(int &position);
+
+    void putPieceEvent();
+
+    void pickOffPieceEvent();
+
+    void solutionFoundEvent();
+    void getPositionInformation(const int &position, int &position_type, int &coord_x, int &coord_y) const;
+    void placerPieceRecursion(int &position, int coord_x, int coord_y, int position_type, Piece &piece_coin);
+};
+
+
+#endif //GENERATOR_H
---
+++ Piece.cpp
@@ -0,0 +1,91 @@
+#include "Piece.h"
+
+Piece::Piece()
+    : couleur{0, 0, 0, 0}
+{
+    id = -1;
+    rotation = 0;
+}
+
+Piece::Piece(const int idPiece, int couleur[4])
+{
+    id = idPiece;
+    rotation = 0;
+    for (int i = 0; i < 4; ++i) {
+        this->couleur[i] = couleur[i];
+    }
+    //Compte le nombre de Zero afin de definir le type
+    int nb_zero = 0;
+
+    for (int i = 0; i < 4; i++) {
+        if (couleur[i] == 0) {
+            nb_zero++;
+        }
+    }
+
+    if (nb_zero == 0) {
+        type = INTERIEUR;
+    } else if (nb_zero == 1) {
+        type = BORD;
+    } else if (nb_zero == 2) {
+        type = COIN;
+    }
+}
+
+const string Piece::toString()
+{
+    ostringstream s;
+    s << id;
+    return s.str();
+}
+
+string Piece::toStringDetail() const
+{
+    ostringstream stream;
+    stream << "Pièce n " << id << endl;
+    return stream.str();
+}
+
+const int Piece::getColor(const int position)
+{
+    if (position > 4) {
+        return -1;
+    }
+
+    return couleur[(position + rotation) % 4];
+}
+
+//Getters
+const int Piece::getRotation()
+{
+    return rotation;
+}
+
+const int *Piece::getTabCouleur()
+{
+    return couleur;
+}
+
+const int Piece::getType()
+{
+    return type;
+}
+
+//Setters
+void Piece::setRotation(const int new_rotation)
+{
+    rotation = new_rotation;
+}
+
+void Piece::setTabCouleur(const int new_tab_couleur[4])
+{
+    for (int i = 0; i < 5; ++i) {
+        couleur[i] = new_tab_couleur[i];
+    }
+}
+
+int Piece::operator[](const int &position)
+{
+    return getColor(position);
+}
+
